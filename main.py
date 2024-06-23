2ccd83faa03c937b40199715c1d68cd6bcd725d3
eddieb <eddiewird@gmail.com>
2024-06-22 21:23:11 -0400

    first try

---
+++ main.py
@@ -0,0 +1,26 @@
+import os 
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
+TOKEN = os.getenv('TOKEN')
+
+client = commands.Bot(command_prefix="/", intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"{client.user.name} is ready")
+
+@client.hybrid_command()
+async def sync(ctx: commands.Context):
+    await ctx.send("Syncing...")
+    await client.tree.sync()
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hey there boyo!")
+
+client.run(token=TOKEN)
