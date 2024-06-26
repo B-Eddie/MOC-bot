6c037b55f13143fb39e826b990b7b650526370b8
eddieb <eddiewird@gmail.com>
2024-06-26 11:59:17 -0400

    omg slash commands work now!!!

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-import os
 import discord
 from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
-intents.message_content = True  # Required for handling slash command interactions
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.tree.command(  # Update here: Use bot.tree.command
-    name="ping",  # Name of the command (what users will type)
-    description="Checks the bot's latency"  # Short description for the command
-)
-async def ping(ctx):
-    await ctx.respond(f"Pong! Latency is {round(bot.latency * 1000)}ms")
+tree = bot.tree
+
+@tree.command(name="info", description="Provides information about the bot")
+async def info(interaction: discord.Interaction):
+    embed = discord.Embed(
+        title="Murder Of Code bot",
+        description="This is the person bot of Murder of Codes!",
+        color=discord.Color.blue()
+    )
+    await interaction.response.send_message(embed=embed)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    await bot.tree.sync()  # Sync commands with Discord
+    await tree.sync()  # Sync commands with Discord
 
-# Retrieve the bot token from environment variable
-bot_token = os.environ.get('TOKEN')
-if bot_token is None:
-    print("Bot token not found in environment variable.")
-else:
-    # Run the bot with the retrieved token
-    bot.run(bot_token)
+bot.run(os.getenv('TOKEN'))
